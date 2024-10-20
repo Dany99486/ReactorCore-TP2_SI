d015195ce62246711a95377f18b1f32d6be76798
Dany99486 <117843005+Dany99486@users.noreply.github.com>
2024-10-20 23:49:52 +0100

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS media (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     release_date DATE NOT NULL,
-    average_rating DECIMAL(3, 2) CHECK (average_rating >= 0 AND average_rating <= 10),
+    average_rating DECIMAL(3, 2) CHECK (average_rating >= 0 AND average_rating <= 10.1),
     type VARCHAR(10) NOT NULL
     );
 
