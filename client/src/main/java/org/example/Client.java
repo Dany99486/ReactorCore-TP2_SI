8d5f24a1055107d118fa926221e6398f955c03dc
Dany99486 <117843005+Dany99486@users.noreply.github.com>
2024-10-22 23:36:54 +0100

    Reorganização e adição do cliente

---
+++ client/src/main/java/org/example/Client.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.example.model.Media;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println("<==== Client ====>");
+
+        String BaseUrl = "http://server:8080";
+        String MyURI = "/media";
+
+        WebClient.create(BaseUrl)
+                .get()
+                .uri(MyURI)
+                .retrieve()
+                .bodyToFlux(Media.class)
+                .subscribe(System.out::println);
+
+        try {
+            Thread.sleep(3*1000);
+        } catch (InterruptedException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
---
+++ client/src/main/java/org/example/model/Media.java
@@ -0,0 +1,20 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Media {
+
+    private Long id; // Identificador da relação
+
+    private String title;
+    private LocalDate releaseDate; // Data de lançamento
+    private double averageRating;    // Classificação média entre 0 e 10
+    private String type; 
+}
