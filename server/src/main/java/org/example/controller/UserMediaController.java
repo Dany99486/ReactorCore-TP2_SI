8c185d17038d60a942382c1996644579f08f9985
Dany99486 <117843005+Dany99486@users.noreply.github.com>
2024-10-31 00:58:37 +0000

    Get UserIDs with MediaIDs and vice-versa

--- server/src/main/java/org/example/controller/UserMediaController.java
+++ server/src/main/java/org/example/controller/UserMediaController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -24,5 +25,17 @@ public class UserMediaController {
     @DeleteMapping("media/{mediaId}/users/{userId}")
     public Mono<Media> removeUserFromMedia(@PathVariable Long mediaId, @PathVariable Long userId) {
         return userMediaService.removeUserFromMedia(mediaId, userId);
+    }
+    @GetMapping("/mediaIds/{userId}")
+    public Flux<Long> getMediaIdsByUserId(@PathVariable Long userId) {
+        return userMediaService.getMediaIdsByUserId(userId)
+                .doOnComplete(() -> logger.info("Successfully fetched media IDs by user ID"))
+                .doOnError(error -> logger.error("Failed to fetch media IDs by user ID", error));
+    }
+    @GetMapping("/userIds/{mediaId}")
+    public Flux<Long> getUserIdsByMediaId(@PathVariable Long mediaId) {
+        return userMediaService.getUserIdsByMediaId(mediaId)
+                .doOnComplete(() -> logger.info("Successfully fetched user IDs by media ID"))
+                .doOnError(error -> logger.error("Failed to fetch user IDs by media ID", error));
     }
 }
--- server/src/main/java/org/example/service/UserMediaService.java
+++ server/src/main/java/org/example/service/UserMediaService.java
@@ -7,6 +7,7 @@ import org.example.repository.UserMediaRepository;
 import org.example.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.HashSet;
@@ -76,5 +77,12 @@ public class UserMediaService {
                                     .then()) // Converte para Mono<Void>
                             .then(mediaRepository.save(media)); // Salva a mídia atualizada
                 });
+    }
+
+    public Flux<Long> getMediaIdsByUserId(Long userId) {
+        return userMediaRepository.findMediaIdsByUserId(userId);
+    }
+    public Flux<Long> getUserIdsByMediaId(Long userId) {
+        return userMediaRepository.findUserIdsByMediaId(userId);
     }
 }
